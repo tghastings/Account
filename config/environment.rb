bb7ceda5c4a3f85d30585c4b8536b29eee237450
Thomas Hastings <thomas@thomashastings.com>
2011-11-14 15:25:19 -0700

    added mailer

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Accounts::Application.initialize!
+
+config.action_mailer.default_url_options = { :host => 'accounts.innopar.com' }
